6fc6c3b30357aaff043f4eccfb960a730f792245
giyeon0312 <giyeon0312@gmail.com>
2022-09-30 18:30:16 +0900

    Add Feature: Removing filled line

--- Tetris/Stage.cpp
+++ Tetris/Stage.cpp
@@ -17,9 +17,33 @@ void CStage::AddBlock(CShape* pCurShape, const POSITION& tPos)
 	{
 		for (int j = 0; j < MAX_SHAPE_SIZE; j++)
 		{
-			// 피봇이 맨 아래 왼쪽이므로 i=0일 때 가장 위다.
 			if (pCurShape->GetBlock(j, i) == '0')
+			{
+				// 피봇이 맨 아래 왼쪽이므로 i=0일 때 가장 위다.
 				m_Stage[tPos.y - (3 - i)][tPos.x + j] = '0';
+			
+				// 현재 줄이 다 차있는지 체크하고 삭제한다.
+				bool bLine = true;
+				for (int k = 0; k < STAGE_WIDTH; ++k)
+				{
+					if (m_Stage[tPos.y - (3 - i)][k] != '0')
+					{
+						bLine = false;
+						break;
+					}
+				}
+				// 위의 블럭들을 모두 한칸씩 내려줘서 지운다.
+				if (bLine)
+				{
+					for (int k = tPos.y - (3 - i); k > 0; --k)
+					{
+						for (int l = 0; l < STAGE_WIDTH; ++l)
+						{
+							m_Stage[k][l] = m_Stage[k - 1][l];
+						}
+					}
+				}
+			}	
 		}
 	}
 }
